35f8b3491a54e12ed43fe6c41ffafee872df42b9
lindsayly <101374847+lindsayly@users.noreply.github.com>
2026-07-13 18:01:04 -0500

    Removing br from sources cell

--- fetch.js
+++ fetch.js
@@ -8,7 +8,7 @@ window.onload = function() {
         for (var i = 0; i < json.length; i++) {
             tableCode += '<tr><td>' + json[i].brand + '</td><td>' + json[i].name + '</td><td>' + json[i].region + '</td><td>' + json[i].notes + '</td></tr>';
     }
-    tableCode += '</tbody><tfoot><tr><td colspan="4">Sources:<br><a href="https://naokimatcha.com/products/superior-blend-matcha>Naoki</a><br><a href="https://namimatcha.com/products/nami-yame-matcha?variant=50295643013412">Nami</a><br><a href="https://ujichamatcha.com/products/samidori-yamamasa-koyamaen?variant=41753933381706">Yamamasa Koyameen</a><br></td></tr></tfoot></table>';
+    tableCode += '</tbody><tfoot><tr><td colspan="4">Sources: <a href="https://naokimatcha.com/products/superior-blend-matcha>Naoki</a> <a href="https://namimatcha.com/products/nami-yame-matcha?variant=50295643013412">Nami</a> <a href="https://ujichamatcha.com/products/samidori-yamamasa-koyamaen?variant=41753933381706">Yamamasa Koyameen</a></td></tr></tfoot></table>';
     document.getElementById('brands').innerHTML = tableCode;
     })
 
